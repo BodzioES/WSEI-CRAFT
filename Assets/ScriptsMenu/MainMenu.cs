4b1c5681ddbdfc329067ba8bc9d9a7842740a12b
BodzioES <114864505+BodzioES@users.noreply.github.com>
2023-04-01 01:52:08 +0200

    bodzio

--- Assets/ScriptsMenu/MainMenu.cs
+++ Assets/ScriptsMenu/MainMenu.cs
@@ -25,6 +25,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
     }
 
+    public void RealGame()
+    {
+        SceneManager.LoadScene(4);
+    }
+
     public void QuitGame()
     {
         Debug.Log("QUIT");
