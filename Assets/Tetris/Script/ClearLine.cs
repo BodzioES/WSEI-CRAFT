275fa4de89df2d10a6eb241fa2a6a4d48688934d
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2023-04-01 09:40:25 +0200

    send

--- Assets/Tetris/Script/ClearLine.cs
+++ Assets/Tetris/Script/ClearLine.cs
@@ -18,10 +18,34 @@ public class ClearLine : MonoBehaviour
             }
         }
         if (all) { return; }
-        for(int i = 0; i< chekers.Length; i++)
+        for (int i = 0; i < chekers.Length; i++)
         {
-            if (chekers[i].gameObject == null) return;
-            chekers[i].check.transform.parent.GetComponent<Tetris>().explode();
+            if (chekers[i].gameObject != null)
+            {
+               
+            }
+        }
+        Transform[] parent = new Transform[chekers.Length];
+        for (int i = 0; i< chekers.Length; i++)
+        {
+            bool s = false;
+            foreach(var a in parent)
+            {
+                if (chekers[i].check.transform.parent == a) { s = true; break; }
+            }
+            if (s)
+            {
+                continue;
+            }
+            else {
+                parent[i] = chekers[i].check.transform;
+            }
+        }
+        foreach (var cheker in parent)
+        {
+            if (cheker.transform.parent == null)
+                continue;
+            cheker.transform.parent.GetComponent<Tetris>().explode();
         }
 
     }
