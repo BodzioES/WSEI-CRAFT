13ef2d6aa5f0612edf5caf7753d43ee510fa7d2a
DIDUS <62933645+DIDUS20@users.noreply.github.com>
2023-04-01 02:23:57 +0200

    Player anim update

--- Assets/Enemies/pacman/PacmanScript.cs
+++ Assets/Enemies/pacman/PacmanScript.cs
@@ -6,6 +6,8 @@ public class PacmanScript : MonoBehaviour
 {
     public GameObject[] players;
 
+    public bool active = false;
+
     public Transform target;
     public int hp;
     public int demage;
@@ -27,58 +29,49 @@ public class PacmanScript : MonoBehaviour
 
     void Update(){
 
-        if(hp <= 0)
+    if (active)
         {
-            Destroy(gameObject);
-        }
-
-        if (players.Length > 1)//Jeœli 2 graczy
-        {
-            float distance1 = Vector2.Distance(players[0].transform.position, gameObject.transform.position);
-            float distance2 = Vector2.Distance(players[1].transform.position, gameObject.transform.position);
-
-            if(distance1 > distance2)
+            if(hp <= 0)
             {
-                target = players[1].transform;
+                Destroy(gameObject);
             }
-            else
-            {
-                target = players[0].transform;
-            }
-        }else //Jeœli 1 gracz
-        {
             target = players[0].transform;
-        }
+   
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-
-        if(Old.x > gameObject.transform.position.x) //left
-        {
-            this.transform.rotation = Quaternion.Euler(0,180,-90);
-        }
-        else if(Old.x < gameObject.transform.position.x) //right
-        {
-            this.transform.rotation = Quaternion.Euler(0, 0, -90);
-        }
-        else if(Old.y > gameObject.transform.position.y) //down
-        {
-            this.transform.rotation = Quaternion.Euler(0, 0, 180);
-        }
-        else if(Old.y < gameObject.transform.position.y) //up
-        {
-            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+            if(Old.x > gameObject.transform.position.x) //left
+            {
+                this.transform.rotation = Quaternion.Euler(0,180,-90);
+            }
+            else if(Old.x < gameObject.transform.position.x) //right
+            {
+                this.transform.rotation = Quaternion.Euler(0, 0, -90);
+            }
+            else if(Old.y > gameObject.transform.position.y) //down
+            {
+                this.transform.rotation = Quaternion.Euler(0, 0, 180);
+            }
+            else if(Old.y < gameObject.transform.position.y) //up
+            {
+                this.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        
+            Old = gameObject.transform.position;
         }
         
-        Old = gameObject.transform.position;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Bullet")
         {
-            hp -= other.gameObject.GetComponent<bullet>().demage;
-            anim.Play("demage");
-            Destroy(other.gameObject);
+            if (active)
+            {
+                hp -= other.gameObject.GetComponent<bullet>().demage;
+                anim.Play("demage");
+                Destroy(other.gameObject);
+            }
+            
         }
     }
 }
