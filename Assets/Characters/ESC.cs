ca46f383b690b2fe29df9db93445a1994bc20bed
DIDUS <62933645+DIDUS20@users.noreply.github.com>
2023-04-01 11:08:55 +0200

    ESC

---
+++ Assets/Characters/ESC.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESC : MonoBehaviour
+{
+
+    public GameObject Menu;
+    bool open = false;
+
+    void Start()
+    {
+        Menu.transform.localScale = new Vector3(1, 0, 1);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape) && open == true) { 
+            Menu.transform.localScale = new Vector3(1, 0, 1);
+            open = false;
+        }else if(Input.GetKey(KeyCode.Escape) && open == false)
+        {
+            Menu.transform.localScale = new Vector3(1, 1, 1);
+            open = true;
+        }
+    }
+}
